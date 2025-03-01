2ec745c005bebbbee788d69a33aa8b0d04a60e70
sophie950 <sueboo950950@gmail.com>
2025-03-01 04:46:51 -0600

    fixing vercel errors with pages and components not exporting as react components.

--- capstone-level3-nextjs/src/pages/controllers/HomePageController.js
+++ capstone-level3-nextjs/src/pages/controllers/HomePageController.js
@@ -1,7 +1,16 @@
-const HomePageController = {
-    handlePageLoad: () => {
+import React, { useEffect } from 'react';
+
+const HomePageController = () => {
+    useEffect(() => {
         console.log("HomePage loaded successfully.");
-    }
+    }, []);
+
+    return (
+        <div>
+            Home Page Content
+        </div>
+    );
 };
 
 export default HomePageController;
+
