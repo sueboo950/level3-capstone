88e81eb47336cf217399b46d3c1e971359727ff1
sueboo950 <155856215+sueboo950@users.noreply.github.com>
2025-03-01 07:53:25 -0600

    Update handlePrayerRequest.js fixing errors

--- capstone-level3-nextjs/src/pages/controllers/handlePrayerRequest.js
+++ capstone-level3-nextjs/src/pages/controllers/handlePrayerRequest.js
@@ -1,61 +1,17 @@
-import React, { useState, useEffect } from 'react';
 import { getBibleVerse } from '../utils/getBibleVerse';
 import { setLastPrayerRequest, getLastPrayerRequest } from '../utils/localStorageUtils';
 
-const PrayerRequestComponent = () => {
-    const [userInput, setUserInput] = useState('');
-    const [result, setResult] = useState('');
-    const [prayerRequest, setPrayerRequest] = useState('');
-
-    const handlePrayerRequest = (userInput) => {
-        setLastPrayerRequest(userInput);
-        const verse = getBibleVerse(userInput);
-        setResult(verse);
-    };
-
-    const loadLastPrayerRequest = () => {
-        const lastPrayerRequest = getLastPrayerRequest();
-        if (lastPrayerRequest) {
-            setPrayerRequest(lastPrayerRequest);
-            const verse = getBibleVerse(lastPrayerRequest);
-            setResult(verse);
-        }
-    };
-
-    useEffect(() => {
-        loadLastPrayerRequest();
-    }, []);
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        handlePrayerRequest(userInput);
-    };
-
-    return (
-        <div>
-            <h2>Prayer Request</h2>
-            <form onSubmit={handleSubmit}>
-                <input
-                    type="text"
-                    value={userInput}
-                    onChange={(e) => setUserInput(e.target.value)}
-                />
-                <button type="submit">Submit</button>
-            </form>
-            {result && (
-                <div>
-                    <h3>Result:</h3>
-                    <p>{result}</p>
-                </div>
-            )}
-            {prayerRequest && (
-                <div>
-                    <h3>Last Prayer Request:</h3>
-                    <p>{prayerRequest}</p>
-                </div>
-            )}
-        </div>
-    );
+export const handlePrayerRequest = (userInput, setResult) => {
+    setLastPrayerRequest(userInput);
+    const verse = getBibleVerse(userInput);
+    setResult(verse);
 };
 
-export default PrayerRequestComponent;
+export const loadLastPrayerRequest = (setPrayerRequest, setResult) => {
+    const lastPrayerRequest = getLastPrayerRequest();
+    if (lastPrayerRequest) {
+        setPrayerRequest(lastPrayerRequest);
+        const verse = getBibleVerse(lastPrayerRequest);
+        setResult(verse);
+    }
+};
