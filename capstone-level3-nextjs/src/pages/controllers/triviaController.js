c9d3dff954ea599acaa97ac4476919c31c40151e
sueboo950 <155856215+sueboo950@users.noreply.github.com>
2025-03-01 05:34:35 -0600

    Update triviaController.js fixing errors

--- capstone-level3-nextjs/src/pages/controllers/triviaController.js
+++ capstone-level3-nextjs/src/pages/controllers/triviaController.js
@@ -1,32 +1,10 @@
-import React, { useState } from 'react';
+// ../controllers/triviaController.js
 
-const QuizComponent = () => {
-    const [score, setScore] = useState(0);
-    const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
-
-    const checkAnswer = (selected, correct) => {
-        if (selected === correct) {
-            setScore((prevScore) => prevScore + 1);
-        }
-        setCurrentQuestionIndex((prevIndex) => prevIndex + 1);
-    };
-
-    // Example question and answer
-    const question = { text: "What is 2 + 2?", options: [2, 3, 4], correct: 4 };
-
-    return (
-        <div>
-            <h2>Quiz</h2>
-            <p>{question.text}</p>
-            {question.options.map((option, index) => (
-                <button key={index} onClick={() => checkAnswer(option, question.correct)}>
-                    {option}
-                </button>
-            ))}
-            <p>Score: {score}</p>
-        </div>
-    );
+export const checkAnswer = (selected, correct) => {
+    if (selected === correct) {
+        setScore((prevScore) => prevScore + 1);
+    }
+    setCurrentQuestionIndex((prevIndex) => prevIndex + 1);
 };
 
-export default QuizComponent;
 
