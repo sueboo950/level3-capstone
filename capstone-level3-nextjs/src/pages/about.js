69f31a8172bc71b8bcefee9dc5b357b6efe773a3
sophie950 <sueboo950950@gmail.com>
2025-03-01 09:04:18 -0600

    attempting to troubleshoot component error in vercel with about page

--- capstone-level3-nextjs/src/pages/about.js
+++ capstone-level3-nextjs/src/pages/about.js
@@ -5,18 +5,46 @@ import AboutContent from '../components/AboutContent';
 import HelpResources from '../components/HelpfulResources';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+class ErrorBoundary extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { hasError: false };
+    }
+
+    static getDerivedStateFromError(error) {
+        // Update state so the next render will show the fallback UI.
+        return { hasError: true };
+    }
+
+    componentDidCatch(error, errorInfo) {
+        // You can also log the error to an error reporting service
+        console.error(error, errorInfo);
+    }
+
+    render() {
+        if (this.state.hasError) {
+            // You can render any custom fallback UI
+            return <h1>Something went wrong.</h1>;
+        }
+
+        return this.props.children;
+    }
+}
+
 const About = () => (
-    <div style={{ backgroundColor: 'violet', height: '100vh' }}>
-        <NavBar />
-        <div className="container" style={{ paddingTop: '5%' }}>
-            <h1>About the Designer</h1>
-            <div className="row">
-                <AboutImage />
-                <AboutContent />
-                <HelpResources />
+    <ErrorBoundary>
+        <div style={{ backgroundColor: 'violet', height: '100vh' }}>
+            <NavBar />
+            <div className="container" style={{ paddingTop: '5%' }}>
+                <h1>About the Designer</h1>
+                <div className="row">
+                    <AboutImage />
+                    <AboutContent />
+                    <HelpResources />
+                </div>
             </div>
         </div>
-    </div>
+    </ErrorBoundary>
 );
 
 export default About;
